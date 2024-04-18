b655cacda42ef05b33aa0e454601b9421ebc16e3
whoisyourvpn <138953036+whoisyourvpn@users.noreply.github.com>
2024-04-17 23:35:56 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -9,8 +9,8 @@ def export_urls_to_txt(db_path, txt_file_path):
     cursor.execute('SELECT url FROM urls')
     urls = cursor.fetchall()  # Fetch all results
     
-    # Write URLs to a text file
-    with open(txt_file_path, 'w') as file:
+    # Write URLs to a text file with UTF-8 encoding
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
         for url in urls:
             file.write(url[0] + '\n')  # url[0] because fetchall returns a list of tuples
     
